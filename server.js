ab3221a9ef3bd211a79b2e57e9806b1109143ff7
unknown <dunghoang210816@gmail.com>
2023-03-06 16:09:00 +0700

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 //console.log('May Node be with you')
 
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient
+const  ObjectID = require('mongodb').ObjectId;
 
 const connectionString = 'mongodb+srv://kanyummi:blader2003@cluster0.fqlgflv.mongodb.net/?retryWrites=true&w=majority'
 
@@ -13,20 +14,20 @@ const connectionString = 'mongodb+srv://kanyummi:blader2003@cluster0.fqlgflv.mon
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
-        
+
         // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
         // -> create -> collection -> 'quotes'
-        const db = client.db('star-wars-quotes')
-        const quotesCollection = db.collection('quotes')
-        
+        const db = client.db('Toy')
+        const quotesCollection = db.collection('products')
+        // To make the 'public' folder accessible to the public
+        app.use(express.static('/public'))
+
         // To tell Express to EJS as the template engine
-        app.set('view engine', 'ejs') 
-        
+        app.set('view engine', 'ejs')
+
         // Make sure you place body-parser before your CRUD handlers!
         app.use(bodyParser.urlencoded({ extended: true }))
 
-        // To make the 'public' folder accessible to the public
-        app.use(express.static('public'))
 
         // To teach the server to read JSON data 
         app.use(bodyParser.json())
@@ -35,36 +36,68 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         // -> server -> '/' -> collection -> 'quotes' -> find() 
         // -> results -> index.ejs -> client
         app.get('/', (req, res) => {
-            db.collection('quotes').find().toArray()
+            db.collection('products').find().toArray()
                 .then(results => {
 
                     // results -> server -> console
                     console.log(results)
-                    
+
                     // results -> index.ejs -> client -> browser 
                     // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
-                    res.render('index.ejs', { quotes: results })
+                    res.render('index.ejs', { toys: results })
                 })
                 .catch(/* ... */)
         })
 
+        app.get('/products', (req, res) => {
+            db.collection('products').find().toArray()
+                .then(results => {
+
+                    // results -> server -> console
+                    console.log(results)
+
+                    // results -> index.ejs -> client -> browser 
+                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
+                    res.render('product.ejs', { toys: results })
+                })
+                .catch(/* ... */)
+        })
+
+        app.get('/add', (req, res) => {
+            res.render('add.ejs')
+        })
+
         // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
         // -> post -> '/quotes' -> collection -> insert -> result
-        app.post('/quotes', (req, res) => {
+        app.post('/save', (req, res) => {
             quotesCollection.insertOne(req.body)
-            .then(result => {
-                
-                // results -> server -> console
-                console.log(result)
+                .then(result => {
 
-                // -> redirect -> '/'
-                res.redirect('/')
-             })
-            .catch(error => console.error(error))
+                    // results -> server -> console
+                    console.log(result)
+
+                    // -> redirect -> '/'
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error))
         })
-        
+
+        app.get('/delete/:id', (req, res) => {
+            let id = req.params.id
+            quotesCollection.deleteOne({ _id: new ObjectID(id) })
+                .then(result => {
+
+                    // results -> server -> console
+                    console.log(result)
+
+                    // -> redirect -> '/'
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error))
+        })
+
         // server -> listen -> port -> 3000
-        app.listen(3000, function() {
+        app.listen(3000, function () {
             console.log('listening on 3000')
         })
     })
